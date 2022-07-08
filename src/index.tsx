0cfa76447e8fdfd1975393dc126fb8cf0cef9cdb
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-07-08 14:53:13 -0700

    update: styling and data rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { ContactCard } from './components/contact-card/ContactCard';
 import reportWebVitals from './reportWebVitals';
+import { contacts } from './contacts'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <>
+        {
+            contacts.map(
+                (contact) =>
+                {
+                    return (
+                        <ContactCard contact={contact} />
+                    );
+                }
+            )
+        }
+        </>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
