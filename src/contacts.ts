24cc9c45e1645979124e85be50f537c4362e8fce
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-07-08 14:47:21 -0700

    add: dummy data

---
+++ src/contacts.ts
@@ -0,0 +1,37 @@
+import { ContactInformation } from "./interface/contact-information.interface";
+
+
+export const contacts: ContactInformation[] = [
+    {
+        unit_number:  '200C',
+        full_name:    'Anton K',
+        category:     'Driver',
+        company_name: 'Force Trans Inc.',
+        phone_number: '+1 (916) 555-3535'
+    },
+    {
+        unit_number:  '328C',
+        full_name:    'Jeremy Lin',
+        category:     'Carrier',
+        company_name: 'USKO',
+        phone_number: '+1 (916) 777-3737'
+    },
+    {
+        full_name:    'Brian Cornell',
+        category:     'Owner',
+        company_name: 'Target Inc.',
+        phone_number: '+1 (800) 333-4422'
+    },
+    {
+        full_name:    'Mike Clarson',
+        category:     'Facility',
+        company_name: 'Amazon',
+        phone_number: '+1 (916) 888-6878'
+    },
+    {
+        full_name:    'Vladimir Lenin',
+        category:     'Other',
+        company_name: 'USSR',
+        phone_number: '+7 (888) 911-1111'
+    },
+]
